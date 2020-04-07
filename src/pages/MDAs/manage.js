b3101e4766bb2b84c694444573a6c83601d2df2a
Domey Benjamin <domeybenjamin1@gmail.com>
2020-04-07 20:10:32 +0000

    Update: added some fixes

--- src/pages/MDAs/manage.js
+++ src/pages/MDAs/manage.js
@@ -48,7 +48,7 @@ const ManageMDAs = props => {
                         Name
                       </th>
                       <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                        Email
+                        Phone
                       </th>
                       <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                         Prefix
@@ -79,12 +79,12 @@ const ManageMDAs = props => {
                                   {/*  : 'N/A'}*/}
                                   {mda.name ? mda.name : mda.name}
                                 </div>
-                                <div className="text-sm leading-5 text-gray-500">{mda.phone ? mda.phone : 'N/A'}</div>
+                                <div className="text-sm leading-5 text-gray-500">{mda.email ? mda.email : 'N/A'}</div>
                               </div>
                             </div>
                           </td>
                           <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                            <div className="text-sm leading-5 text-gray-900">{mda.email ? mda.email : 'N/A'}</div>
+                            <div className="text-sm leading-5 text-gray-900">{mda.phone ? mda.phone : 'N/A'}</div>
                           </td>
                           <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
                             {mda.prefix ? mda.prefix : 'N/A'}
